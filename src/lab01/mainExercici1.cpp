61520df08b3ec363c73d25de9fd59a81befd71e4
David Blandón <djesus1906@protonmail.com>
2023-02-20 14:39:16 +0100

    Lab01: Ex1 and Ex2

---
+++ src/lab01/mainExercici1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    string name;
+    int option;
+    string arr_options[] = {"Sortir", "Benvinguda"};
+    cout << "Hola com et dius? ";
+    cin >> name;
+    do
+    {
+        cout << "Hola " << name << ", que vols fer ?" << endl;
+        for (int i = 0; i < 2; i++)
+        {
+            cout << i + 1 << "." << arr_options[i] << endl;
+        }
+        cin >> option;
+        if (option == 1)
+        {
+            cout << "Fins a la propera " << name << endl;
+        }
+        else
+        {
+            cout << "Benvinguda a l'asignatura d'estructures de dades " << name << endl;
+        }
+
+    } while (option != 1);
+
+    return 0;
+}
---
+++ src/lab02/mainExercici2.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void show_menu(int &option, vector<string> options)
+{
+    for (int i = 0; i < options.size(); i++)
+    {
+        cout << i + 1 << "." << options[i] << endl;
+    }
+    cin >> option;
+}
+void get_name(string &name)
+{
+    cout << "Hola com et dius? ";
+    cin >> name;
+}
+int main(int argc, char const *argv[])
+{
+    string name;
+    int option;
+    vector<string> arr_options{"Sortir", "Benvinguda", "Redefinir nom"};
+    get_name(name);
+    do
+    {
+        cout << "Hola " << name << ", que vols fer ?" << endl;
+        show_menu(option, arr_options);
+        switch (option)
+        {
+        case 1:
+            cout << "Fins a la propera " << name << endl;
+            break;
+        case 2:
+            cout << "Benvinguda a l'asignatura d'estructures de dades " << name << endl;
+            break;
+        case 3:
+            get_name(name);
+            break;
+        }
+
+    } while (option != 1);
+
+    return 0;
+}
